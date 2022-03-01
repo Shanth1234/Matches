c91c0239954fc90561ae92b5044c86d933da0b50
Shanth1234 <96223229+Shanth1234@users.noreply.github.com>
2022-03-01 21:42:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ import pandas as pd
 app = dash.Dash(__name__)
 server = app.server
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/srinathkr07/IPL-Data-Analysis/master/matches.csv')
 
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+fig = px.bar(df, x="season", y="winner",
                  size="population", color="continent", hover_name="country",
                  log_x=True, size_max=60)
 
 app.layout = html.Div([
     dcc.Graph(
-        id='life-exp-vs-gdp',
+        id='IPL',
         figure=fig
     )
 ])
